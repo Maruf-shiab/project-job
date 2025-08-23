a918066aedc7967ba1c2f5bc9c4678fe013f8490
Mushfiq-Srijon <mushfiq.srijon@gmail.com>
2025-08-23 23:14:53 +0600

    Added pagination logic

--- src/components/JobListing.jsx
+++ src/components/JobListing.jsx
@@ -1,12 +1,14 @@
 import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
-import { JobCategories, JobLocations } from '../assets/assets';
+import { assets, JobCategories, JobLocations } from '../assets/assets';
 import JobCard from './JobCard';
 
 const JobListing = () => {
-    const { isSearched, searchFilter, setSearchFilter ,jobs } = useContext(AppContext);
+    const { isSearched, searchFilter, setSearchFilter, jobs } = useContext(AppContext);
 
-    const[showFilter,setShowFilter] = useState(false);
+    const [showFilter, setShowFilter] = useState(false);
+
+    const [currentPage, setCurrentPage] = useState(1);
 
     // Simple X icon as SVG
     const CloseIcon = ({ onClick }) => (
@@ -52,7 +54,7 @@ const JobListing = () => {
                     <h4 className='font-medium text-lg py-4'>Search by Categories</h4>
                     <ul className='space-y-4 text-gray-600'>
                         {
-                            JobCategories.map((category, index)=>(
+                            JobCategories.map((category, index) => (
                                 <li className='flex gap-3 items-center' key={index}>
                                     <input className='scale-125' type="checkbox" name="" id="" />
                                     {category}
@@ -68,7 +70,7 @@ const JobListing = () => {
                     <h4 className='font-medium text-lg py-4 pt-14'>Search by Locations</h4>
                     <ul className='space-y-4 text-gray-600'>
                         {
-                            JobLocations.map((location, index)=>(
+                            JobLocations.map((location, index) => (
                                 <li className='flex gap-3 items-center' key={index}>
                                     <input className='scale-125' type="checkbox" name="" id="" />
                                     {location}
@@ -81,13 +83,31 @@ const JobListing = () => {
             </div>
             {/*job listings section*/}
             <section className="w-full lg:w-3/4 text-gray-800 max-lg:px-4 ">
-                <h3 className='font-medium text-3xl py-2 'id ='job-list'>Latest Jobs</h3>
+                <h3 className='font-medium text-3xl py-2 ' id='job-list'>Latest Jobs</h3>
                 <p className='mb-8'>Ready for a career glow-up? ✨</p>
                 <div className='grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-4'>
-                  {jobs.map((job,index) => (
-                   <JobCard key={index} job={job} />
-                  ))}  
+                    {jobs.slice((currentPage - 1) * 6, currentPage * 6).map((job, index) => (
+                        <JobCard key={index} job={job} />
+                    ))}
                 </div>
+
+                {/* Pagination */}
+                {jobs.length > 0 && (
+                    <div className='flex items-center justify-center space-x-2 mt-10'>
+                        <a href="#job-list">
+                            <img onClick={() => setCurrentPage(Math.max(currentPage - 1, 1))} src={assets.left_arrow_icon} alt="" />
+                        </a>
+                        {Array.from({ length: Math.ceil(jobs.length / 6) }).map((_, index) => (
+                            <a href="#job-list">
+                                <button onClick={() => setCurrentPage(index + 1)} className={`w-10 h-10 flex items-center justify-center border border-gray-300 rounded cursor-pointer ${currentPage === index + 1 ? 'bg-blue-100 text-blue-500' : 'text-gray-500'}`}>{index + 1}</button>
+                            </a>
+                        ))}
+                        <a href="#job-list">
+                            <img onClick={() => setCurrentPage(Math.min(currentPage + 1, Math.ceil(jobs.length / 6)))} src={assets.right_arrow_icon} alt="" />
+                        </a>
+                    </div>
+                )}
+
             </section>
         </div>
     );
