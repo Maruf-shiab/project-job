1cfa542bae0bd0898e846f0a00215edc03bcad76
SabikunEthika <alam.sabikun2001@gmail.com>
2025-09-03 14:36:16 +0600

    applyJob-2

--- src/pages/ApplyJob.jsx
+++ src/pages/ApplyJob.jsx
@@ -66,11 +66,13 @@ function ApplyJob() {
               <p className='mt-1 text-gray-600'>Posted {moment(JobData.date).fromNow()}</p>
             </div>
           </div>
-          <div>
-            <div>
-              <h2>Job Description</h2>
-              <div dangerouslySetInnerHTML={{__html:JobData.description}}></div>
-                <button>Apply Now</button>
+          <div className='flex flex-col lg:flex-row justify-between items-start'>
+            <div className=' w-full lg:w-2/3'>
+              <h2 className='font-bold text-2xl mb-4 text-purple-950'>Job Description</h2>
+              <div className='rich-text' dangerouslySetInnerHTML={{__html:JobData.description}}></div>
+                <button className='bg-blue-600 p-2,5 px-10 text-white rounded mt-10'>
+                Apply Now
+              </button>
 
               
             </div>
