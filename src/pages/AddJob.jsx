99a96b61456337e46be62eaa16bc3e78102198d8
Mushfiq-Srijon <mushfiq.srijon@gmail.com>
2025-09-14 01:20:53 +0600

    Users can now post job and the data will go to the database. Only the posted jobs will show on the manage jobs page

--- src/pages/AddJob.jsx
+++ src/pages/AddJob.jsx
@@ -1,6 +1,11 @@
-import React, { useState,useRef, useEffect } from 'react'
+import React, { useState,useRef, useEffect, useContext } from 'react'
 import Quill from 'quill';
 import { JobCategories, JobLocations } from '../assets/assets';
+import axios from 'axios';
+import { AppContext } from '../context/AppContext';
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 
 const AddJob = () => {
 
@@ -13,6 +18,34 @@ const AddJob = () => {
    const editorRef=useRef(null)
    const quillRef=useRef(null)
 
+   const {backendUrl, companyToken} = useContext(AppContext)
+
+   const onSubmitHandler = async(e) => {
+    e.preventDefault()
+
+    try {
+      
+      const description = quillRef.current.root.innerHTML
+
+      const {data} = await axios.post(backendUrl+'/api/company/post-job',
+        {title,description,location,salary,category,level},
+        {headers: {token:companyToken}}
+      )
+
+      if (data.success) {
+        toast.success(data.message)
+        setTitle('')
+        setSalary(0)
+        quillRef.current.root.innerHTML = ""
+      } else {
+        toast.error(data.message)
+      }
+
+    } catch (error) {
+      toast.error(error.message)
+    }
+   }
+
    useEffect(()=>{
     //initiate quill only once
     if(!quillRef.current && editorRef.current){
@@ -20,9 +53,9 @@ const AddJob = () => {
         theme:'snow',
       })
     }
-   })
+   },[])
    return (
-    <form className='container p-4 flex flex-col w-full items-start gap-3'>
+    <form onSubmit={onSubmitHandler} className='container p-4 flex flex-col w-full items-start gap-3'>
       <div className='w-full'>
         <p className='mb-2 font-bold text-2xl text-blue-950'>Job Title </p>
         <input type="text" placeholder='Type Here'
